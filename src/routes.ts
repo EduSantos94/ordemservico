8112b2ae268fd82a63d041430388f45f05fefed6
Eduardo <eduardo.santos@fastcore.com.br>
2024-03-01 13:52:37 -0300

    adiciona novas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,10 @@ import companies from '@/controllers/companies'
 import plans from '@/controllers/plans'
 import products from '@/controllers/products'
 import users from '@/controllers/users'
-import services from '@/controllers/services'
-import serviceProducts from '@/controllers/serviceProducts'
-import serviceImages from '@/controllers/serviceImages'
+import orders from '@/controllers/orders'
+import orderProducts from '@/controllers/orderProducts'
+import orderImages from '@/controllers/orderImages'
+import orderServices from '@/controllers/orderServices'
 import login from '@/controllers/login'
 import { verifyJWT } from './utils'
 
@@ -15,13 +16,14 @@ routes.get('/', (req, res) => {
   res.json({"API": "API NODE"})
 })
 
-routes.use('/companies', companies.router)
+routes.use('/companies', verifyJWT,companies.router)
 routes.use('/login', login.router)
 routes.use('/plans', plans.router)
-routes.use('/products', products.router)
+routes.use('/products', verifyJWT,products.router)
 routes.use('/users', verifyJWT, users.router)
-routes.use('/services', services.router)
-routes.use('/serviceProducts', serviceProducts.router)
-routes.use('/serviceImages', serviceImages.router)
+routes.use('/orders', verifyJWT,orders.router)
+routes.use('/orderProducts', verifyJWT,orderProducts.router)
+routes.use('/ordemImages', verifyJWT,orderImages.router)
+routes.use('/orderServices', verifyJWT,orderServices.router)
 
 export default routes
